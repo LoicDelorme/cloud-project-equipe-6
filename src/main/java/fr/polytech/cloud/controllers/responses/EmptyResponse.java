e12224d755f827713739eb8c059a878b1cfff123
Nicolas <nicoloco@laposte.net>
2017-11-21 20:00:40 +0100

    Removing auto-detection of getters to prevent issues with Json responses

--- src/main/java/fr/polytech/cloud/controllers/responses/EmptyResponse.java
+++ src/main/java/fr/polytech/cloud/controllers/responses/EmptyResponse.java
@@ -1,5 +1,7 @@
 package fr.polytech.cloud.controllers.responses;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE)
 public class EmptyResponse {
 
 }
